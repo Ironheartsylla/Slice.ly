e7afa259ece0be1cd05c2de9fa12785cdb9043b0
Ironheartsylla <kiankyle1201@gmail.com>
2025-07-06 19:46:27 +0800

    Refactor: Update database configuration to use PostgreSQL

--- pages/api/shorten.ts
+++ pages/api/shorten.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+
 const prisma = new PrismaClient();
 
 export default async function handler(
